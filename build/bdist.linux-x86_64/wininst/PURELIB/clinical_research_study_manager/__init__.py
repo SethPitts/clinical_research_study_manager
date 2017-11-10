b74cc92ea432b51755c71d487d5c1a6bbf871c82
beliefs22 <beliefs22@gmail.com>
2017-11-10 16:34:56 -0500

    Updated Builds and version number

--- build/bdist.linux-x86_64/wininst/PURELIB/clinical_research_study_manager/__init__.py
+++ build/bdist.linux-x86_64/wininst/PURELIB/clinical_research_study_manager/__init__.py
@@ -1,1 +1,5 @@
+from . import study_manager
 
+# is this the correct way to be able to have it work by calling clinical_research_study_manager without having to call
+# study manager also?
+study_manager.start()
